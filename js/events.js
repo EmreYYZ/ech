c83e1854b4e261b60ab90361ec575a2d24d9d788
Adam Ali <adamalexali@Adams-MacBook-Pro.local>
2020-02-18 10:55:05 -0500

    testing setSrc

--- js/events.js
+++ js/events.js
@@ -1,19 +1,19 @@
-AFRAME.registerComponent("markerhandler", {
-  init: function() {
-    var imagePlane = document.getElementById("dogPlane");
-    var secondSrc = "img/Burger_Sticker.jpg";
+// AFRAME.registerComponent("markerhandler", {
+//   init: function() {
+//     var imagePlane = document.getElementById("dogPlane");
+//     var secondSrc = "img/Burger_Sticker.jpg";
 
-    // every click, we make our model grow in size :)
-    // animatedMarker.addEventListener("click", function(ev, target) {
-    //   const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-    //   if (aEntity && intersectedElement === aEntity) {
-    //     const scale = aEntity.getAttribute("scale");
-    //     Object.keys(scale).forEach(key => (scale[key] = scale[key] + 1));
-    //     aEntity.setAttribute("scale", scale);
-    //   }
-    // });
-    var setAtt = (document.querySelector("body").onclick = function() {
-      imagePlane.setAttribute("src", secondSrc);
-    });
-  }
-});
+//     // every click, we make our model grow in size :)
+//     // animatedMarker.addEventListener("click", function(ev, target) {
+//     //   const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+//     //   if (aEntity && intersectedElement === aEntity) {
+//     //     const scale = aEntity.getAttribute("scale");
+//     //     Object.keys(scale).forEach(key => (scale[key] = scale[key] + 1));
+//     //     aEntity.setAttribute("scale", scale);
+//     //   }
+//     // });
+//     var setAtt = (document.querySelector("body").onclick = function() {
+//       imagePlane.setAttribute("src", secondSrc);
+//     });
+//   }
+// });
