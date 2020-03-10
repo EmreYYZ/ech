a057e2c3581938212561338e9c0cc7bca42bd2a8
Adam Ali <adamalexali@Adams-MacBook-Pro.local>
2020-03-10 09:11:54 -0400

    trying to set src when marker is visible

--- js/events.js
+++ js/events.js
@@ -1,32 +1,39 @@
-AFRAME.registerComponent("markerhandler", {
-  init: function() {
-    const animatedMarker = document.querySelector("#animated-marker");
-    var imagePlane = document.getElementById("dogPlane");
-    var secondSrc = "img/Burger_Sticker.jpg";
-
-    // every click, we make our model grow in size :)
-    animatedMarker.addEventListener("click", function(ev, target) {
-      const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-      if (imagePlane && intersectedElement === imagePlane) {
-        // const scale = aEntity.getAttribute("scale");
-        // Object.keys(scale).forEach(key => (scale[key] = scale[key] + 1));
-        // aEntity.setAttribute("scale", scale);
-        imagePlane.setAttribute("src", `${secondSrc}`);
-      }
-    });
-  }
-});
-
 // AFRAME.registerComponent("markerhandler", {
 //   init: function() {
+//     const animatedMarker = document.querySelector("#animated-marker");
 //     var imagePlane = document.getElementById("dogPlane");
 //     var secondSrc = "img/Burger_Sticker.jpg";
-//   },
-//   tick: function() {
-//     if (document.querySelector("a-marker").object3D.visible == false) {
-//       imagePlane.setAttribute("src", `${secondSrc}`)
-//     } else {
-//       console.log("else reached");
-//     }
+
+//     // every click, we make our model grow in size :)
+//     animatedMarker.addEventListener("click", function(ev, target) {
+//       const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+//       if (imagePlane && intersectedElement === imagePlane) {
+//         // const scale = aEntity.getAttribute("scale");
+//         // Object.keys(scale).forEach(key => (scale[key] = scale[key] + 1));
+//         // aEntity.setAttribute("scale", scale);
+//         imagePlane.setAttribute("src", `${secondSrc}`);
+//       }
+//     });
 //   }
 // });
+
+let assets = [
+  {
+    id: "dog",
+    src: "img/dog.jpg"
+  },
+  {
+    id: "burger",
+    src: "img/Burger_Sticker.jpg"
+  }
+];
+
+AFRAME.registerComponent("markerhandler", {
+  tick: function() {
+    if (document.querySelector("a-marker").object3D.visible == true) {
+      document.querySelector("a-plane").setAttribute("src", `${assets[1].src}`)
+    } else {
+      
+    }
+  }
+});
